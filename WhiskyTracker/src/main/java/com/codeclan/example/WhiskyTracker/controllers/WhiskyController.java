7ef8013450cba48f0abbdf734b7f63243ffb5386
MikhailGorbunov <68015812+MikhailGorbunov@users.noreply.github.com>
2023-02-01 16:03:46 +0000

    WHISKY routes

--- WhiskyTracker/src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ WhiskyTracker/src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -6,4 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WhiskyController {
 
+    @Getmapping(value = "/whiskies")
+    public responseEntity<List<Whisky>> getAllWhiskies(){
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.Ok);
+    }
+    @Getmapping(value = "/whiskies/{id}")
+    public responseEntity<Whisky> getWhisky(@PathVariable Long id){
+        return new ResponseEntity<>(whiskyRepository.findById(id), HttpStatus.Ok);
+    }
+
+    @PostMapping(value = "/whiskies")
+    public repsonseEntity<Whisky> postWhisky(@RequestBody Whisky whisky){
+        whiskyRepository.save(whisky);
+        return new ResponseEntity<>(whisky, HttpStatus.CREATED);
+    }
+
 }
